0f3907e6aa59776c3a410f5795a5c13c7c542a15
Anton Simonov <simonov.aa@outlook.com>
2024-11-22 23:03:54 +0300

    Added navigation to the map

--- PathfindingCityMap.cpp
+++ PathfindingCityMap.cpp
@@ -69,7 +69,7 @@ struct RoadHandler : public osmium::handler::Handler
             {
                 osmium::Location loc = it->second;
 
-                if (BOX_SOCHI.contains(loc)) 
+                if (BOX_BOLSHOY_SOCHI.contains(loc)) 
                 {
                     intersectsBox = true;
                 }
@@ -128,22 +128,92 @@ struct RoadHandler : public osmium::handler::Handler
             }
         }
     }
-
-    private:
-        bool checkIntersection(const osmium::Box &box)
-        {
-            return false;
-        }
 };
 
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-const int MAP_OFFSET_FROM_BOUND = 1;
+int WINDOW_WIDTH = 800;
+int WINDOW_HEIGHT = 600;
+float zoom = 1.0f;
+float offsetX = 0.0f;
+float offsetY = 0.0f;
+bool isDragging = false;
+double lastMouseX = 0.0, lastMouseY = 0.0;
 
-glm::vec2 loc2Screen(const osmium::Location &location, const osmium::Box &bounds)
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-    double u = (location.lon() - bounds.right()) / (bounds.left() - bounds.right()) * WINDOW_WIDTH;
-    double v = (bounds.top()   - location.lat()) / (bounds.top() - bounds.bottom()) * WINDOW_HEIGHT;
+    if (button == GLFW_MOUSE_BUTTON_LEFT)
+    {
+        if (action == GLFW_PRESS) 
+        {
+            isDragging = true;
+            glfwGetCursorPos(window, &lastMouseX, &lastMouseY);
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            isDragging = false;
+        }
+    }
+}
+
+void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (isDragging)
+    {
+        double dx = xpos - lastMouseX;
+        double dy = ypos - lastMouseY;
+
+        offsetX -= dx / zoom;
+        offsetY += dy / zoom;
+
+        lastMouseX = xpos;
+        lastMouseY = ypos;
+    }
+}
+
+void cursorToWorld(GLFWwindow* window, double cursorX, double cursorY, float& worldX, float& worldY) {
+    // Переводим экранные координаты в диапазон [-1, 1]
+    float normalizedX = (2.0f * cursorX) / WINDOW_WIDTH - 1.0f;
+    float normalizedY = 1.0f - (2.0f * cursorY) / WINDOW_HEIGHT; // Y инвертирован в OpenGL
+
+    // Переводим нормализованные координаты в мировое пространство
+    worldX = normalizedX * (WINDOW_WIDTH / 2.0f / zoom) + offsetX;
+    worldY = normalizedY * (WINDOW_HEIGHT / 2.0f / zoom) + offsetY;
+}
+
+//Зум относительно фиксированной точки
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    double cursorX, cursorY;
+    glfwGetCursorPos(window, &cursorX, &cursorY);
+
+    // Преобразуем позицию курсора в мировые координаты до изменения зума
+    float worldXBefore, worldYBefore;
+    cursorToWorld(window, cursorX, cursorY, worldXBefore, worldYBefore);
+
+    float oldZoom = zoom;
+    zoom *= (yoffset > 0) ? 1.1f : 0.9f;
+    zoom = std::max(0.01f, std::min(zoom, 100.0f));
+
+    // Преобразуем позицию курсора в мировые координаты после изменения зума
+    float worldXAfter, worldYAfter;
+    cursorToWorld(window, cursorX, cursorY, worldXAfter, worldYAfter);
+
+    // Корректируем смещение, чтобы курсор оставался в том же месте в мировых координатах
+    offsetX += (worldXBefore - worldXAfter);
+    offsetY += (worldYBefore - worldYAfter);
+}
+
+//Для поддержки изменения размера окна
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    WINDOW_WIDTH = width;
+    WINDOW_HEIGHT = height;
+    glViewport(0, 0, width, height);
+}
+
+glm::vec2 locToScreen(const osmium::Location &location, const osmium::Box &bounds)
+{
+    double u = (bounds.left() -  location.lon()) / (bounds.left() - bounds.right()) * WINDOW_WIDTH;
+    double v = (location.lat() - bounds.bottom()) / (bounds.top() - bounds.bottom()) * WINDOW_HEIGHT;
 
     return glm::vec2(u, v);
 }
@@ -155,11 +225,11 @@ std::vector<float> prepareGraphVertices(const osmium::Box &bounds)
     for (const auto& [nodeID, edges] : graph) 
     {
 
-        glm::vec2 nodeFrom = loc2Screen(nodeLocations[nodeID], bounds);
+        glm::vec2 nodeFrom = locToScreen(nodeLocations[nodeID], bounds);
 
         for (const auto& edge : edges) 
         {
-            glm::vec2 nodeTo = loc2Screen(nodeLocations[edge.nodeToID], bounds);
+            glm::vec2 nodeTo = locToScreen(nodeLocations[edge.nodeToID], bounds);
 
             vertices.push_back(nodeFrom.x);
             vertices.push_back(nodeFrom.y);
@@ -185,6 +255,11 @@ void initOpenGL(GLFWwindow*& window) {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetScrollCallback(window, scrollCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetCursorPosCallback(window, cursorPositionCallback);
+
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to initialize GLEW\n";
         exit(EXIT_FAILURE);
@@ -196,7 +271,6 @@ void initOpenGL(GLFWwindow*& window) {
     glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, -1, 1);
 }
 
-// Отрисовка графа
 void renderGraph(GLFWwindow* window, const std::vector<float>& vertices) {
     GLuint VBO;
     glGenBuffers(1, &VBO);
@@ -204,8 +278,22 @@ void renderGraph(GLFWwindow* window, const std::vector<float>& vertices) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
+    float lastFrameTime = glfwGetTime();
+
     while (!glfwWindowShouldClose(window)) {
+        float currentFrameTime = glfwGetTime();
+        float deltaTime = currentFrameTime - lastFrameTime;
+        lastFrameTime = currentFrameTime;
+
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-0.5f * WINDOW_WIDTH / zoom + offsetX, 
+                0.5f * WINDOW_WIDTH  / zoom + offsetX, 
+               -0.5f * WINDOW_HEIGHT / zoom + offsetY, 
+                0.5f * WINDOW_HEIGHT / zoom + offsetY, 
+               -1.0f, 1.0f);
 
         glEnableClientState(GL_VERTEX_ARRAY);
         glVertexPointer(2, GL_FLOAT, 0, nullptr);
@@ -233,11 +321,21 @@ int main(int argc, char** argv)
 
         osmium::io::Reader nodeReader{inputFile, osmium::osm_entity_bits::node};
         NodeHandler nodeHandler;
+
+        std::cout << "\nStarted writing nodes\n";
+
         osmium::apply(nodeReader, nodeHandler);
+
+        std::cout << "\nCompleted writing nodes\n";
 
         RoadHandler roadHandler;
         osmium::io::Reader roadReader{inputFile, osmium::osm_entity_bits::way};
+
+        std::cout << "\nConstructing graph\n";
+
         osmium::apply(roadReader, roadHandler);
+
+        std::cout << "\nCompleted constructing graph\n";
 
         const osmium::MemoryUsage memory;
 
